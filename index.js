7f4f42289330e7dd76425151d6a232fe3ef04d26
INDRA\ljhidalgo <ljhidalgo@indracompany.com>
2021-03-26 19:19:39 -0500

    ajustes para deployment 2

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended:true }));
 
 // Puerto de la app
-const port = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -30,5 +30,5 @@ app.get('/', (req,res) => {
 
 // Arrancar la app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor está funcionando en el puerto ${PORT}`);
+    console.log(`El servidor está funcionando en el puerto ${port}`);
 });
